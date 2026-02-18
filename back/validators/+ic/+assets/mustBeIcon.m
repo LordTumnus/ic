a6c6385e023af4b4bed8345d9f6aef445eeb502c
Antonio <a.hortalsegura@gmail.com>
2026-02-18 21:57:24 +0100

    🎨 add asset validators

---
+++ back/validators/+ic/+assets/mustBeIcon.m
@@ -0,0 +1,23 @@
+function mustBeIcon(asset)
+   % > MUSTBEICON Validates that an asset is a valid icon source.
+   %
+   % Use as a property validator after implicit conversion:
+   %   Source ic.Asset {ic.assets.mustBeIcon} = ic.Asset("info")
+   %
+   % Accepts:
+   %   - empty: no icon
+   %   - name: Lucide icon name (resolved on frontend)
+   %   - file: must be .svg
+   %   - url: must end in .svg (query string stripped)
+   %
+   % Rejects file/url assets with non-SVG extensions.
+   if asset.Type == "" || asset.Type == "name"
+      return
+   end
+   [~, ~, ext] = fileparts(asset.Value);
+   ext = lower(regexprep(ext, '\?.*$', ''));
+   if ext ~= ".svg"
+      error('ic:assets:InvalidIcon', ...
+         'Icon source must be .svg, got "%s"', ext);
+   end
+end
---
+++ back/validators/+ic/+assets/mustBeImage.m
@@ -0,0 +1,32 @@
+function mustBeImage(asset)
+   % > MUSTBEIMAGE Validates that an asset is a valid image source.
+   %
+   % Use as a property validator after implicit conversion:
+   %   Source ic.Asset {ic.assets.mustBeImage} = ic.Asset()
+   %
+   % Accepts:
+   %   - empty: no image
+   %   - file: must have image extension
+   %
+   % Rejects name-type assets (Lucide icons are not images).
+   if asset.Type == ""
+      return
+   end
+   if asset.Type == "name"
+      error('ic:assets:InvalidImage', ...
+         'Image source must be a file or URL, not a name ("%s")', ...
+         asset.Value);
+   end
+   if asset.Type == "url"
+      return
+   end
+   % file → check extension
+   validExts = [".png", ".jpg", ".jpeg", ".gif", ".bmp", ...
+                ".webp", ".svg", ".ico"];
+   [~, ~, ext] = fileparts(asset.Value);
+   if ~ismember(lower(ext), validExts)
+      error('ic:assets:InvalidImage', ...
+         'Image must be one of %s, got "%s"', ...
+         strjoin(validExts, ", "), ext);
+   end
+end
